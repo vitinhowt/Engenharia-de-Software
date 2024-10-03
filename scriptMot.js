569a37f5c600c1e3a63ad9ffa75426baf3d6f362
Vitor Sousa <vitor23070209@aluno.cesupa.br>
2024-10-03 00:23:30 -0300

    Add files via upload

---
+++ scriptMot.js
@@ -0,0 +1,10 @@
+function acceptRide() {
+    alert("Corrida aceita! Direcionando ao ponto de partida...");
+    // Lógica para aceitar a corrida, como redirecionar o motorista
+    // para a localização do passageiro.
+}
+
+function declineRide() {
+    alert("Corrida recusada.");
+    // Lógica para recusar a corrida, como notificar o sistema.
+}
